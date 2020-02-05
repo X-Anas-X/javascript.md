afb23158308b25682781e4b357918501a9942d67
X-Anas-X <anas.zain1996@gmail.com>
2020-02-05 21:57:54 +0200

    hi

---
+++ lab07.js
@@ -0,0 +1,23 @@
+prompt("who is this?")
+prompt("have you eaten today?")
+prompt("how was your exam?")
+alert("we will do well")
+var today = new Date();
+var hourNow =today.getHours();
+var greetings;
+if (hourNow>18){
+    greeting='Good evening!';
+
+}
+else if (hourNow>12){
+    greeting='Good afternoon!';
+
+}
+else if (hourNow>0){
+    greeting='Good morning!';
+
+}
+else {
+    greeting='Welcome!';
+}
+document.write('<h3>'+greeting+'</h3>');
